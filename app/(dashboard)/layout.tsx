716d4e6735353ac3dac7e3bc30c928dc31252e50
Fadi-Faiyad <zenfaiyad@gmail.com>
2023-09-17 19:18:28 -0700

    223

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+const DashbordLayout = ({ children }) => {
+    return <div>
+        <h1> DashBored</h1>
+        <div>{children}</div>
+    </div>
+}
+
+
+export default DashbordLayout
---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,6 @@
+console.log(process.argv)
+
+const TodoPage = () => {
+ return <div> Todo Page </div>
+}
+export default TodoPage
